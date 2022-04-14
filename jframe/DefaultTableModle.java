62f130d5799fc0505b5d743a804e628f083d1620
Dharmik13 <88429448+Dharmik13@users.noreply.github.com>
2022-04-14 22:41:12 +0530

    Add files via upload

---
+++ jframe/DefaultTableModle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jframe;
+
+/**
+ *
+ * @author dharmik
+ */
+class DefaultTableModle {
+
+    void addRow(Object[] obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setRowCount(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
